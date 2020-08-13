8bbae4b3544d7eb6f6ecfb44f76d57dd7379561d
mac <mac@macs-MacBook-Air.local>
2020-08-13 14:35:24 -0700

    add ppconfig, routes

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+// const db = require('./models');
+const db = require('../models')
 
 router.get('/signup', (req, res) => {
   res.render('auth/signup');
@@ -8,5 +10,34 @@ router.get('/signup', (req, res) => {
 router.get('/login', (req, res) => {
   res.render('auth/login');
 });
+
+router.post('/signup', (req, res) => {
+  console.log(req.body);
+  db.user.findOrCreate({
+    where: {
+      email: req.body.email
+    },
+    defaults: {
+      name: req.body.name,
+      password: req.body.password
+    }
+  })
+  .then(([user, created]) => {
+    if(created){
+      // if created, success and redirect
+      console.log(`${user.name} was created`)
+      res.redirect('/');
+    } else{
+      // Email already exists
+      console.log('Email already exists');
+      res.redirect('/auth/signup');
+    }
+  })
+  .catch(err => {
+    console.log('error', err);
+    res.redirect('/auth/signup');
+  })
+
+})
 
 module.exports = router;
